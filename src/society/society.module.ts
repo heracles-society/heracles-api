26d787e9b7643d94ac7e9edb9da81226cb5aaecc
Avik <sayavik@gmail.com>
2020-06-08 03:21:10 +0530

    Society Module updated with Nested Modules like inventory, event, complaint

--- src/society/society.module.ts
+++ src/society/society.module.ts
@@ -4,9 +4,18 @@ import { SocietyService } from './society.service';
 import { SocietyProvider } from './society.provider';
 import { DatabaseModule } from '../database/database.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { ComplaintModule } from './complaint/complaint.module';
+import { EventModule } from './event/event.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @Module({
-  imports: [DatabaseModule, ReservationModule],
+  imports: [
+    DatabaseModule,
+    ReservationModule,
+    ComplaintModule,
+    EventModule,
+    InventoryModule,
+  ],
   controllers: [SocietyController],
   providers: [SocietyService, ...SocietyProvider.getProviders()],
 })
