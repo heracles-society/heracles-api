0f66dbb4ae0970f86ea9d49b6ade5fd8070aafb5
Avik Sarkar <sayavik@gmail.com>
2020-06-14 23:01:09 +0530

    Removed older order module

--- src/orders/constant.ts
+++
@@ -1,4 +0,0 @@
-export const ORDERS_MODEL = 'Orders';
-export const ORDERS_PROVIDER = `${ORDERS_MODEL.toUpperCase()}_MODEL`;
-export const TRANSACTION_SUCCESS = 'captured';
-export const TRANSACTION_FAILURE = 'failed';
