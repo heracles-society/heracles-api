5e65a5330ed0e931ddc17a177983ac900bdc964c
Avik Sarkar <sayavik@gmail.com>
2020-06-14 23:05:36 +0530

    Removed redundant module dependancy from inventory module

--- src/society/inventory/inventory.module.ts
+++ src/society/inventory/inventory.module.ts
@@ -1,13 +1,12 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { UserModule } from '../../user/user.module';
 import { DatabaseModule } from '../../database/database.module';
 import { InventoryProvider } from './inventory.provider';
 import { InventoryController } from './inventory.controller';
-import { SocietyModule } from '../society.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, forwardRef(() => SocietyModule)],
+  imports: [UserModule, DatabaseModule],
   providers: [InventoryService, ...InventoryProvider.getProviders()],
   exports: [InventoryService],
   controllers: [InventoryController],
